db654ab8cdd467153a2d1e670d9040d09294e169
Yusuf Adeleke <yadeleke@my.centennialcollege.ca>
2025-04-07 12:24:28 -0400

    Added database access message. Closes #2

--- obj/Debug/net8.0/yusu_quiz4_dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/yusu_quiz4_dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("yusu_quiz4_dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c4a67720a64c68a715e5e83bb5c0823518c8bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("yusu_quiz4_dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("yusu_quiz4_dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
