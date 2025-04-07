6679de4cbc7af184dceceaaf7c614984b493295b
Yusuf Adeleke <yadeleke@my.centennialcollege.ca>
2025-04-07 12:19:54 -0400

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace yusu_quiz4_dotnet.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        ViewData["Message"] = "Database access has already been done on April 1, 2025 by George Wong";
+    }
+
+}
